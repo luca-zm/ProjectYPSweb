a823b03d615f2e467de59305152e1b45bea99b0b
luca-zm <55975243+luca-zm@users.noreply.github.com>
2020-02-05 12:37:29 +0100

    selenium luca

---
+++ src/selenium/Luca/testSelenium1.java
@@ -0,0 +1,31 @@
+package selTest;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testSelenium1 {
+	public static void main(String []args) {
+		
+		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost:8080/webzeco/index.jsp");
+		//email
+		driver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("ttt");
+		//password
+		driver.findElement(By.xpath("//*[@id=\"psw\"]")).sendKeys("ttt");
+		//submit email and password
+		driver.findElement(By.xpath("/html/body/section/div/div/div[1]/div/form/button")).click();
+		
+		//click on userprofile navbar
+		driver.findElement(By.xpath("/html/body/nav/div/form/ul/li[4]/a")).click();
+		
+		//check name on userprofile --> ttt
+		WebElement TxtBoxContent = driver.findElement(By.xpath("/html/body/section[1]/div/div/div[1]/div/div/p[1]"));
+
+     	System.out.println(TxtBoxContent.getText());
+		driver.close();
+	}
+}
